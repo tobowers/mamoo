25b505968058e97db5847f6d02a2beb3f773bec4
Topper Bowers <topper@toppingdesign.com>
2011-04-15 15:20:28 -0400

    early days, but maybe make this a functional commonjs project

---
+++ vows/controller.test.js
@@ -0,0 +1,24 @@
+var vows = require('vows'),
+    assert = require('assert');
+require("../lib/index");
+
+var MBX = global.MBX;
+
+vows.describe("a controller").addBatch({
+    "creating a model": {
+        topic: function () {
+            var topic = this;
+            var MyModel = MBX.JsModel.create("MyModel");
+            var controller = MBX.JsController.create("MyModelController", {
+                model: MyModel,
+                onInstanceCreate: function (obj) {
+                    topic.callback(null, obj, MyModel);
+                }
+            });
+            MyModel.create()
+        },
+        "should get the object": function (err, obj, model) {
+            assert.equal(obj.parentClass.modelName, model.modelName);
+        }
+    }
+}).export(module);
